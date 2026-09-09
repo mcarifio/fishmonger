1d2fe93de5871a51bf96a9e87da3a9e62ea8cf59
Mike Carifio <mike@carif.io>
2026-09-09 10:37:23 -0400

    bootstrap ollama

--- bootstrap.d/ollama.fish
+++ bootstrap.d/ollama.fish
@@ -5,8 +5,7 @@ set -l cmd (path basename --no-extension (status filename))
 command? $cmd; or die "$cmd not found"
 
 set -l models  "$HOME/.config/ollama/models"
-mkdir -p "$models" >&2
-Ux OLLAMA_MODELS="$models"
+mkdir -p "$models" &>/dev/null; and Ux OLLAMA_MODELS="$models"
 
 [ "$(brew --prefix ollama)" = "$(command -s ollama)" ]; and brew services start ollama
 
