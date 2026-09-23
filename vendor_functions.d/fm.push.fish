96b6599cfdb788eeda66c51ef1c2dd3595067299
Mike Carifio <mike@carif.io>
2026-09-23 11:31:55 -0400

    log to stderr

--- vendor_functions.d/fm.push.fish
+++ vendor_functions.d/fm.push.fish
@@ -1,4 +1,7 @@
 function (fn (status filename))
+    # @summary: push fishmonger changes to git repo
+    # @usage: fm.push "great additions!"
+    
     argparse -us -- $argv
     set -f git_message "$(status function) commit"
     if [ -n "$argv[1]" ]
@@ -6,7 +9,10 @@ function (fn (status filename))
 	set -e argv[1]
     end
     set -f C "$(path dirname (status filename))"
-    git -C "$C" add ..
-    git -C "$C" commit -am "$git_message"
-    git -C "$C" push
+    begin
+	git -C "$C" add ..	     
+	git -C "$C" commit -am "$git_message"
+	git -C "$C" log -n1 >&2
+	git -C "$C" push
+    end >&2
 end
